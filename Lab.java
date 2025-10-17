b46449b81db0a930c918c898093389afc19dda4c
rorasan97-oss <rorasan97@gmail.com>
2025-10-17 16:51:37 +0300

    Update and rename Raneem.java to Lab.java

---
+++ Lab.java
@@ -0,0 +1,18 @@
+public class Lab 
+{
+private boolean hasProject;
+public Lab (boolean hasProject , String courseID , String courseName , double Grade){
+super (String courseID , String courseName , double Grade);
+this.hasProject = hasProject;
+}
+@Override
+public String toString(){
+return super.toString() + " Does the Lab course have a a project? " + ( hasProject? " There is a project": "");
+}
+public double calculateGrade(){
+double totalGrade = Grade;
+if (hasProject == true)
+totalGrade = totalGrade + 10;
+return totalGrade;
+}
+}
