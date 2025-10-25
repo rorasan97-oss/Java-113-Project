ef136c8a51cdc422f380e4037021163298ce7231
rorasan97-oss <rorasan97@gmail.com>
2025-10-25 22:46:47 +0300

    Add files via upload

---
+++ VideoGame.java
@@ -0,0 +1,30 @@
+public class VideoGame extends Products
+{
+private boolean Status;
+private char type;
+public VideoGame (char type, String ProductName , double price){
+super (ProductName , price);
+this.type = type;
+}
+public VideoGame(VideoGame v) {
+	super(v.ProductName, v.price);
+	this.type = v.type;
+}
+@Override
+public String toString(){
+return super.toString() + ", Game Type:" +"("+ type+")";
+}
+public double calculatePrice(){
+double totalprice = price;
+if ( type==('D') || type==('d')){
+totalprice = totalprice - 0.50 * totalprice;
+}
+return totalprice;
+}
+public void setType(char type){
+this.type = type;
+}
+public char getType(){
+return type;
+}
+}
